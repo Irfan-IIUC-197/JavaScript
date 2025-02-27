912a1288a82a39f52302d879e7b8895bc876886d
Irfan-IIUC-197 <mi2670952@gmail.com>
2025-02-27 23:39:33 +0600

    New file

---
+++ New.js
@@ -0,0 +1,11 @@
+lastdigit = 7
+
+lastdigitplusone = 8
+
+console.log("Next integer of my last digit of my id : ", lastdigitplusone)
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
